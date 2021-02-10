93b5347c111c89250b7d8e4335062d6fdfcc598c
E99p1ant <i@github.red>
2021-02-11 00:44:49 +0800

    sandbox: docker container error

--- public/js/sandbox.js
+++ public/js/sandbox.js
@@ -1,7 +1,7 @@
 $('#run').click(() => {
     $('#result_bar').css('color', 'white');
     $('#result_data').text('Loading...');
-    
+
     $.post('', editor.getValue(), (res) => {
         let result = atob(res.data.result.body);
         let exitCode = res.data.result.exit_code;
@@ -9,5 +9,8 @@ $('#run').click(() => {
             $('#result_bar').css('color', 'red');
         }
         $('#result_data').text(result);
+    }).fail((err) => {
+        $('#result_bar').css('color', 'red');
+        $('#result_data').text(err.responseJSON.msg);
     })
 })
