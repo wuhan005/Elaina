59d3f14303969ed2087bbf6b876bac8b46258b64
E99p1ant <i@github.red>
2021-02-10 12:33:37 +0800

    fe: init

---
+++ frontend/src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: function (h) { return h(App) }
+}).$mount('#app')
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {}
+}
+</script>
