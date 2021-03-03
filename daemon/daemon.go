cf50ab0d33e58f386abcfbb248257928b935121a
E99p1ant <i@github.red>
2021-03-04 00:45:00 +0800

    daemon: update callback

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"os/exec"
 	"path"
@@ -31,13 +32,19 @@ func main() {
 		}
 		err := c.BindJSON(&input)
 		if err != nil {
-			c.JSON(gadget.MakeErrJSON(40000, "Daemon: unexpected input"))
+			c.JSON(http.StatusOK, gin.H{
+				"error":  true,
+				"stderr": "Daemon: unexpected input",
+			})
 			return
 		}
 
 		err = ioutil.WriteFile(path.Join("/runtime/runner", input.FileName), []byte(input.Data), 0644)
 		if err != nil {
-			c.JSON(gadget.MakeErrJSON(400, err.Error()))
+			c.JSON(http.StatusOK, gin.H{
+				"error":  true,
+				"stderr": err.Error(),
+			})
 			return
 		}
 		c.JSON(gadget.MakeSuccessJSON("success"))
@@ -62,13 +69,15 @@ func main() {
 		err = cmd.Run()
 
 		c.JSON(gadget.MakeSuccessJSON(gin.H{
+			"error":  err != nil,
 			"stdout": stdOut.String(),
 			"stderr": stdErr.String(),
-			"error":  err != nil,
 		}))
 	})
 
-	err := r.RunUnix("/runtime/elaina-daemon.sock")
+	unixSocket := "./elaina-daemon.sock"
+	err := r.RunUnix(unixSocket)
+	defer os.Remove(unixSocket)
 	if err != nil {
 		log.Fatal("Failed to start daemon: %v", err)
 	}
