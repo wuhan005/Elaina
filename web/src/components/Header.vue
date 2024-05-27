7d36d63c76105f39f63852c99176bb47be85d5c5
E99p1ant <i@github.red>
2024-05-27 21:39:43 +0800

    web: sign out

--- web/src/components/Header.vue
+++ web/src/components/Header.vue
@@ -23,11 +23,13 @@
       Sandbox
     </t-menu-item>
     <template #operations>
-      <t-button variant="text" shape="square">
-        <template #icon>
-          <t-icon name="ellipsis"/>
-        </template>
-      </t-button>
+      <t-dropdown :options="[{ content: 'Sign Out', value: 'sign-out' }]" @click="onClickDropDown">
+        <t-button variant="text" shape="square">
+          <template #icon>
+            <t-icon name="ellipsis"/>
+          </template>
+        </t-button>
+      </t-dropdown>
     </template>
   </t-head-menu>
 </template>
@@ -36,13 +38,25 @@
 import {ref} from 'vue'
 import {useRoute, useRouter} from 'vue-router'
 import {MenuValue} from "tdesign-vue-next";
+import {DropdownOption} from "tdesign-vue-next";
+import {useAuthStore} from "@/store";
 
 const route = useRoute()
 const router = useRouter()
 const menuValue = ref(route.name)
+const authStore = useAuthStore()
 
 const onChange = (value: MenuValue) => {
   router.push({name: value.toString()})
+}
+
+const onClickDropDown = (dropdownItem: DropdownOption, context: { e: MouseEvent }) => {
+  switch (dropdownItem.value) {
+    case 'sign-out':
+      authStore.cleanToken()
+      router.push({name: 'signIn'})
+      break
+  }
 }
 </script>
 
